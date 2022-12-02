4b1a37837aba47868ae281f7e9e2ce02ae2203f3
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2022-12-02 08:33:17 -0800

    day 2 part 2

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -9,8 +9,7 @@ fun main() {
         var score = 0
         for (match in input) {
             val (me, them) = match.split(" ")
-            println("me: $me")
-            println("them: $them")
+
             when (them) {
                 "X" ->  {
                     score += 1
@@ -46,7 +45,42 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var score = 0
+        for (match in input) {
+            val (me, them) = match.split(" ")
+
+            when (them) {
+                "X" ->  {
+                    score += 0
+                    score += when (me) {
+                        "A" -> 3
+                        "B" -> 1
+                        "C" -> 2
+                        else -> throw Exception("Invalid type: $me")
+                    }
+                }
+                "Y" -> {
+                    score += 3
+                    score += when (me) {
+                        "A" -> 1
+                        "B" -> 2
+                        "C" -> 3
+                        else -> throw Exception("Invalid type: $me")
+                    }
+                }
+                "Z" -> {
+                    score += 6
+                    score += when (me) {
+                        "A" -> 2
+                        "B" -> 3
+                        "C" -> 1
+                        else -> throw Exception("Invalid type: $me")
+                    }
+                }
+                else -> throw Exception("Invalid type: $them")
+            }
+        }
+        return score
     }
     val testInput = readInput("day2/test")
     check(part1(testInput) == 15)
