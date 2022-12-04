dc18b1c7bd92f81d59e3f2fe8bd5d44de6f93bfa
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2022-12-04 09:02:22 -0800

    day 4 part 2

--- src/day4/Day4.kt
+++ src/day4/Day4.kt
@@ -9,11 +9,9 @@ fun main() {
     fun part1(input: List<String>): Int {
         var total = 0
         for (line in input) {
-            println("loop")
             val (first, second) = line.split(",")
             val (firstFirst, firstSecond) = first.split("-").map(String::toInt)
             val (secondFirst, secondSecond) = second.split("-").map(String::toInt)
-            println("$firstFirst, $firstSecond, $secondFirst, $secondSecond")
             if ((firstFirst >= secondFirst && firstSecond <= secondSecond) || (firstFirst <= secondFirst && firstSecond >= secondSecond)) {
                 total++
             }
@@ -22,11 +20,22 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var total = 0
+        for (line in input) {
+            val (first, second) = line.split(",")
+            val (firstFirst, firstSecond) = first.split("-").map(String::toInt)
+            val (secondFirst, secondSecond) = second.split("-").map(String::toInt)
+            if ((firstFirst..firstSecond).toList().intersect(   (secondFirst..secondSecond).toList().toSet()).isNotEmpty()) {
+                total++
+                println("Yes: $firstFirst, $firstSecond, $secondFirst, $secondSecond")
+            } else {
+                println("No: $firstFirst, $firstSecond, $secondFirst, $secondSecond")
+            }
+        }
+        return total
     }
 
     val testInput = readInput("day4/test")
-    println(part1(testInput))
     check(part1(testInput) == 2)
     val input = readInput("day4/input")
     println(part1(input))
